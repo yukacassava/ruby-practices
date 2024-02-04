f2f37c97bbf58d0db6c40ab97522815210816b67
yukacassava <68782731+yukacassava@users.noreply.github.com>
2024-02-04 21:09:15 +0900

    Update fizzbuzz.rb
    do-end間にインデントをつけた

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,15 @@
 a = 0
 20.times do 
-a = a + 1
-if a % (3*5) == 0
-  puts "FizzBuzz"
-elsif a % 5 == 0
-  puts "buzz"
-elsif a % 3 == 0 
-  puts "Fizz"
-elsif
-  puts a 
-end
+  a = a + 1
+  if a % (3*5) == 0
+    puts "FizzBuzz"
+  elsif a % 5 == 0
+    puts "buzz"
+  elsif a % 3 == 0 
+    puts "Fizz"
+  elsif
+    puts a 
+  end
 end
 
  
