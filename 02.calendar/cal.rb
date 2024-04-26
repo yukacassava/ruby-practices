05ae76e599c5d9865eba2f37d7887e295b4dbde2
yukacassava <bu11ers_ltf@yahoo.co.jp>
2024-04-26 16:48:08 +0900

    カレンダー問題再々提出

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -35,27 +35,26 @@ first_day = Date.new(year, month, 1)
 #指定月の最終日を取得
 last_day = Date.new(year, month, -1)
 
-#月の日数を取得
-days_in_month = (first_day.next_month - first_day).to_i
-
 #初日の曜日を取得
 first_day_of_week = first_day.wday
 
 #出力するdayのフォーマットを共通化
 def format_day(day)
-    day.day.to_s.rjust(2)
+  day.day.to_s.rjust(2)
 end
 
 #　初日の曜日までスペースを表示
 print "   " * first_day_of_week
 
 (first_day..last_day).each do |day|
-  if Date.today.year == year && Date.today.month == month && Date.today.day == day.day
+  formatted_day = format_day(day)
+  if Date.today == day
      print "\e[30;42m#{format_day(day)}\e[0m" + " "
   else
-     print format_day(day) + " " 
+     print formatted_day + " " 
   end
-  puts if (first_day_of_week + day.day) % 7 == 0
+  puts if day.saturday? 
 end
 
-puts 
+puts
+
